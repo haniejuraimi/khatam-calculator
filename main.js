0d5e6e60afa9f45314629569efc19677b75e85cf
haniejuraimi <haniealia08@gmail.com>
2019-05-25 16:04:51 +0800

    Add for days
    I added the codes for the days too

--- main.js
+++ main.js
@@ -15,7 +15,41 @@ function calculateDaysNeeded(){
 
     // get the element of the card
     const resultArea = document.getElementById('result')
+    
+    let message = 'Please input a valid number of pages'
 
+    if (pageInputValue && pageInputValue > 0 && pageInputValue < 605) {
+        message = 'If you read  ' + pageInputValue + ' page(s) a day, you will khatam it in about ' + niceResult + '  day(s).'
+    }
     // output the result on the card
-    resultArea.innerHTML = 'If you read  ' + pageInputValue + ' page(s) a day, you will complete it in about ' + niceResult + '  days .'
+    resultArea.innerHTML = message
 }
+
+function calculatePagesNeeded(){
+    
+    // get the value from the input field
+
+    const dayInputElement = document.getElementById('first_name3')
+    const dayInputValue = dayInputElement.value
+    //  const dayInputValue = document.getElementById('first_name3').value //
+    console.log(dayInputValue)
+
+    // use formula to find day needed
+    let total = 604
+    let result = total / dayInputValue
+    const niceResult = result.toFixed(2)
+    console.log('result', result)
+
+    // get the element of the card
+    const resultArea = document.getElementById('result1')
+    let message = 'Please input a valid number of day'
+
+    if (dayInputValue && dayInputValue > 0) {
+        message = 'If you read  for ' + dayInputValue + ' day(s), you will complete reading about ' + niceResult + '  pages(s).'
+    }
+    // output the result on the card
+    resultArea.innerHTML = message
+}
+
+
+
